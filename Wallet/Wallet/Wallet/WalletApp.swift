c6861a09da70a3f929516db84b2ef3a6afd65f68
Taeyoun Lee <sino.lty@gmail.com>
2022-03-06 23:01:03 +0900

    Create Wallet Project

---
+++ Wallet/Wallet/Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Taeyoun Lee on 2022/03/06.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
