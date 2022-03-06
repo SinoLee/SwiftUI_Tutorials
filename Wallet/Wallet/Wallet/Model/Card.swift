c2cff7cb393201bc2a829bf745f6d5ada08b2311
Taeyoun Lee <sino.lty@gmail.com>
2022-03-06 23:06:49 +0900

    [Wallet] Add Files

---
+++ Wallet/Wallet/Wallet/Model/Card.swift
@@ -0,0 +1,20 @@
+//
+//  Card.swift
+//  Wallet
+//
+//  Created by Taeyoun Lee on 2022/03/06.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Card_Previews: PreviewProvider {
+    static var previews: some View {
+        Card()
+    }
+}
---
+++ Wallet/Wallet/Wallet/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Wallet
+//
+//  Created by Taeyoun Lee on 2022/03/06.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
